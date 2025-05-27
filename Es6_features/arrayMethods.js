13bd2e00e0cf0c3befbfbab998d0695e9c720f91
Roshan Bhagat <raushan@medianv.com>
2025-05-27 12:34:54 +0530

    added array methods

---
+++ Es6_features/arrayMethods.js
@@ -0,0 +1,108 @@
+// Push method
+let teamMembers = ["Rahul","Vishal","Raman"]
+// for adding any new member to the list we can use push method that adds the provided element to the last 
+teamMembers.push("Vishnu")
+console.log(teamMembers) // [ 'Rahul', 'Vishal', 'Raman', 'Vishnu' ]
+
+
+// pop for removing the element from last
+teamMembers.pop()
+console.log(teamMembers) // [ 'Rahul', 'Vishal', 'Raman' ]
+
+
+
+// includes method
+const allowedRoles = ["admin","developer","owner","manager"]
+const userRole = "developer"
+
+if(allowedRoles.includes(userRole)){
+    console.log("Grant Permission!")
+}
+
+
+// sort method
+
+// it changes the original array (in-place)
+let productsList= [
+    {name:"Bag",price:450},
+    {name:"Shoes",price:1000},
+    {name:"Books",price:250},
+    {name:"Clothes",price:700}
+]
+
+console.log('productsList: ', productsList);
+
+const sortedProductList = productsList.sort((a,b)=>a.price - b.price)
+
+console.log('productsList: ', productsList);
+console.log('SortedproductsList: ', sortedProductList);
+
+
+
+
+// find method : finds the first instance from the array
+const users = [
+  { name: "Vikash", role: "user" },
+  { name: "Arjun", role: "admin" },
+  { name: "Shivani", role: "admin" },
+];
+
+const firstAdmin = users.find(user => user.role === "admin");
+console.log(firstAdmin); // { name: "Arjun", role: "admin" }
+
+
+
+// map is also called a higher order function 
+// it returns a new array means does'nt modifies the original array
+const pricesInUSD = [10, 20, 30];
+const convirsionRate = 85.35;
+
+const pricesInINR = pricesInUSD.map(price => price * convirsionRate);
+console.log(pricesInINR); // [ 853.5, 1707, 2560.5 ]
+
+
+
+
+// filter 
+const products = [
+  { name: "Laptop", inStock: true },
+  { name: "Phone", inStock: false },
+  { name: "Tablet", inStock: true },
+];
+
+const availableProducts = products.filter((product)=>{
+    return product.inStock
+});
+console.log(availableProducts);
+// [ { name: 'Laptop', inStock: true }, { name: 'Tablet', inStock: true } ]
+
+
+
+// reduce method
+
+let cart = [
+    {productName:"Bag",Price:400},
+    {productName:"Book",Price:700},
+    {productName:"Bottal",Price:250},
+    {productName:"Pen",Price:500}
+]
+
+const getTotalPrice = cart.reduce((total,pro)=>{
+
+   total += pro.Price  
+   return total
+},0)
+
+console.log(getTotalPrice)
+
+
+
+
+
+
+
+
+
+
+
+
