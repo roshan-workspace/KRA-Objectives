87c27e1078b60fe1d880617d90e61da7f8a01e38
Roshan <roshanking021@gmail.com>
2025-05-27 10:55:12 +0530

    first commit

---
+++ Es6_features/Basics.js
@@ -0,0 +1,16 @@
+// working with let and const variables
+
+let myName  = "Roshan"
+myName  = "Sandeep" // this will work
+// let myName  = "Ram"  // can't do this, this will throw an error 
+
+console.log(age) // error : cannot access before initialization
+let age =  21
+
+
+
+// for const variable 
+// const adhaarNo = 123456789123   // const is used for assigning the values that does not change over time
+// const adhaarNo = 645184515454   // cannot redeclare any variable in the same scope 
+
+adhaarNo = 222333444555   // this gies an error Assignment to constant variable 
