b44e1e19dbf5801bfccbe4424e1605ef40d11d6b
Roshan Bhagat <raushan@medianv.com>
2025-05-30 13:24:54 +0530

    pacticing nest

--- NestJs_Basics/basic_01/src/users/users.controller.ts
+++ NestJs_Basics/basic_01/src/users/users.controller.ts
@@ -15,7 +15,7 @@ export class UsersController {
         }
 
         @Get(':id')  // GET users/:id
-        findOne(@Param("id", ParseIntPipe) id: number){
+        findOne(@Param("id", ParseIntPipe) id: number){ 
             return this.userService.findOne(id)
         }
 
