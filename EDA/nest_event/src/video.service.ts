de011fb6f13281bc099502e6c2a343da5703d244
roshan-workspace <raushan@medianv.com>
2025-06-30 12:16:36 +0530

    event emitter with nestjs

---
+++ EDA/nest_event/src/video.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ViewerService } from './viewer.service';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+@Injectable()
+export class VideoService {
+constructor(private eventEmitter: EventEmitter2) {}
+
+    async publish(){
+        const title = "How to get Rich within a month";
+        console.log('Publishing a new Video');
+
+        const result = await this.eventEmitter.emitAsync('video.created', {title})
+
+        return {message:'All Done', result};
+    }
+
+}
