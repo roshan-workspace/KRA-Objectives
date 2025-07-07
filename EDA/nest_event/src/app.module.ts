a47f9ef6a7c90155e5ea24b97cf9161c02941d40
roshan-workspace <raushan@medianv.com>
2025-07-07 18:06:46 +0530

    git

--- EDA/nest_event/src/app.module.ts
+++ EDA/nest_event/src/app.module.ts
@@ -1,14 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationShutdown } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoService } from './video.service';
 import { ViewerService } from './viewer.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AdvertismentService } from './advertisment.service';
+import { single } from 'rxjs';
+import { DiscoveryModule } from '@nestjs/core';
 
 @Module({
-  imports: [EventEmitterModule.forRoot()],
+  imports: [EventEmitterModule.forRoot(), DiscoveryModule],
   controllers: [AppController],
   providers: [AppService, VideoService,ViewerService, AdvertismentService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationShutdown {
+  onApplicationShutdown(signal?: string) {
+    console.log("Shutdown", signal);
+  }
+}
--- EDA/nest_event/src/app.service.ts
+++ EDA/nest_event/src/app.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { DiscoveryModule } from '@nestjs/core';
 
 @Injectable()
 export class AppService {
+  constructor(private discoverableModule:DiscoveryModule){
+    
+  }
   getHello(): string {
     return 'Hello World!';
   }
